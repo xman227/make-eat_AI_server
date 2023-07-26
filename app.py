ff6c099398ade8ce2c348bd3030e2af1284d0689
xman227 <x22z@naver.com>
2023-07-26 12:53:17 +0900

    restart

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def detection():
 
 if __name__ == '__main__':
     #webbrowser.open()    #webbrowser.open('http://127.0.0.1:5000/')
-    app.run(host='0.0.0.0')            
+    app.run(host='0.0.0.0', port=5000)            
     #app.run(host='127.0.0.1', port=5000)
