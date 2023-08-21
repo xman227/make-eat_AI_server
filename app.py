cd626689d56db7ea6238f9acaf25f0a1da554b44
xman227 <x22z@naver.com>
2023-08-21 15:17:41 +0900

    extract model

--- app.py
+++ app.py
@@ -11,20 +11,16 @@ with open(file_path, encoding='utf-8') as f:
 app = Flask(__name__)
 
 use_count = [0]
+device = torch.device('cpu')
+# 업로드한 pt model 주소
+model = torch.hub.load('ultralytics/yolov5', 'custom', './best.pt',force_reload=False)  
+model.to(device)
 
 @app.route("/image", methods=['POST'])
 def test():
     if request.method == 'POST':
-
-        if use_count[0] == 0:
-            device = torch.device('cpu')
-            # 업로드한 pt model 주소
-            model = torch.hub.load('ultralytics/yolov5', 'custom', './best.pt',force_reload=False)  
-            model.to(device)
-        
         
         use_count[0] += 1
-
         # 결과 반환
         file = request.files['image'] #print(f'사진의 이름과 형태는 : {file}')
         results = model(Image.open(file))
